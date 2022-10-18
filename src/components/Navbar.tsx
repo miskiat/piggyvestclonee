ee67383dd70750c316254612042cfbbc9ce2fd3c
Miskiat Badmus <badmusmiskiat@gmail.com>
2022-10-18 12:35:58 +0100

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/logo_08e6965627/logo_08e6965627.svg" />
+
+      <span>Save</span>
+      <span>Invest</span>
+      <span>Stories</span>
+      <span>FAQs</span>
+      <span>Blog</span>
+      <button>Sign in</button>
+      <button>Create free account</button>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home/Index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.scss";
+import Navbar from "../../components/Navbar";
+
+const Index = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Index;
