f7beb4ebf161695563196a573796c8c320974ea5
Miskiat Badmus <badmusmiskiat@gmail.com>
2022-10-19 23:53:29 +0100

    hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <div>The Better Way to Save & Invest.</div>
+      <div>
+        PiggyVest helps over 4 million customers achieve their financial goals
+        by helping them save and invest with ease.
+      </div>
+      <button>Create free account</button>
+      <div>
+        <button>
+          <img
+            src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/apple_icon_32ccc572ff/apple_icon_32ccc572ff.svg"
+            alt="download-apple"
+            width="18"
+            height="22"
+          />
+          Get on iphone
+        </button>
+        <button>
+          <img
+            src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/google_icon_9867fc9be6/google_icon_9867fc9be6.svg"
+            alt="download-google"
+            width="24"
+            height="24"
+          />
+          Get on Andriod
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Home.scss";
 import Navbar from "../../components/Navbar";
+import Hero from "../../components/Hero";
 
 const Index = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 };
