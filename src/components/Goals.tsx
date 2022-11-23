8985ed50eb182e8933002268c99c9be5bb30a47c
Miskiat Badmus <badmusmiskiat@gmail.com>
2022-11-23 21:23:59 +0100

    goals section

---
+++ src/components/Goals.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Goals = () => {
+  return (
+    <div className="goals">
+      <div>
+        <img
+          src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/security_369a26d96e/security_369a26d96e.png"
+          alt="security icon"
+        />
+        <div>Your Security is our priority</div>
+        <div>
+          PiggyVest uses the highest level of Internet Security and it is
+          secured by 256 bits SSL security encryption to ensure that your
+          information is comepletely protected from fraud.
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Goals;
--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./Home.scss";
 import Navbar from "../../components/Navbar";
 import Hero from "../../components/Hero";
+import Goals from "../../components/Goals";
 
 const Index = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Goals />
     </div>
   );
 };
