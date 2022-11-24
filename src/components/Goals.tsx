bd26b7257bb5bcfbb0cd62af186f98aebde1ec85
Miskiat Badmus <badmusmiskiat@gmail.com>
2022-11-24 23:17:10 +0100

    badge

--- src/components/Goals.tsx
+++ src/components/Goals.tsx
@@ -3,25 +3,27 @@ import React from "react";
 const Goals = () => {
   return (
     <div className="goals">
-      <div>
+      <div className="security-badge">
         <img
+          className="badge"
           src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/security_369a26d96e/security_369a26d96e.png"
           alt="security icon"
         />
-        <div>Your Security is our priority</div>
         <div>
-          PiggyVest uses the highest level of Internet Security and it is
-          secured by 256 bits SSL security encryption to ensure that your
-          information is comepletely protected from fraud.
+          <div>Your Security is our priority</div>
+          <div>
+            PiggyVest uses the highest level of Internet Security and it is
+            secured by 256 bits SSL security encryption to ensure that your
+            information is comepletely protected from fraud.
+          </div>
+          <button>learn more</button>
         </div>
-        <button>lern more ></button>
       </div>
       <div>
-          <div>4 ways to build your savings</div>
-          <div>Earn 5%-15% when you save with any of these PiggyVest plans.</div>
-          <button>Start Saving</button>
+        <div>4 ways to build your savings</div>
+        <div>Earn 5%-15% when you save with any of these PiggyVest plans.</div>
+        <button>Start Saving</button>
       </div>
-
     </div>
   );
 };
