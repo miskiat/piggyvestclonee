2a366ac0744e42cf2da6c3727dea70d587a61927
Miskiat Badmus <badmusmiskiat@gmail.com>
2022-11-26 00:18:57 +0100

    goals

--- src/components/Goals.tsx
+++ src/components/Goals.tsx
@@ -19,10 +19,70 @@ const Goals = () => {
           <button className="learn">Learn more </button>
         </div>
       </div>
-      <div>
-        <div>4 ways to build your savings</div>
-        <div>Earn 5%-15% when you save with any of these PiggyVest plans.</div>
-        <button>Start Saving</button>
+      <div className="investment">
+        <div className="growth">
+          <h1>4 ways to build your savings</h1>
+          <p>Earn 5%-15% when you save with any of these PiggyVest plans.</p>
+          <button className="saving">Start Saving</button>
+        </div>
+        <div className="plans">
+          <div className="piggybank">
+            <img
+              src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/piggy_df88262015/piggy_df88262015.png"
+              class="icon"
+              width="50"
+              height="50"
+            ></img>
+            <h2>Automated Savings</h2>
+            <p className="build">
+              Build a dedicated savings faster on your terms automatically or
+              manually.
+            </p>
+            <div className="arrow">Piggybank</div>
+          </div>
+          <div className="piggybank">
+            <img
+              src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/safelock_4e737ba7d3/safelock_4e737ba7d3.png"
+              class="icon"
+              width="50"
+              height="50"
+            />
+            <h2>Fixed Savings</h2>
+            <p className="build">
+              Lock money away for a fixed duration with no acess to it until
+              maturity. It's like having a custom fixed deposit.
+            </p>
+            <div className="arrow">Savelock</div>
+          </div>
+          <div className="piggybank">
+            <img
+              src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/target_6c049f3410/target_6c049f3410.png"
+              class="icon"
+              width="50"
+              height="50"
+            />
+            <h2>Goal-oriented Savings</h2>
+            <p className="build">
+              Reach all your savings goals faster. Save towards multiple goals
+              on your own or with a group.
+            </p>
+            <div className="arrow">Target Savings</div>
+          </div>
+          <div className="piggybank">
+            <img
+              src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/flex_d090f8bce2/flex_d090f8bce2.png"
+              class="icon"
+              width="50"
+              height="50"
+            />
+            <h2>Flexible Savings</h2>
+            <p className="build">
+              Save,transfer, withdraw,manage and organise your money for free at
+              any time.
+            </p>
+            <div className="arrow">Flex Naira</div>
+          </div>
+        </div>
       </div>
     </div>
   );
