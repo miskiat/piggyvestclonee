839640e9c452a361312c3965b1b14b0d4253d023
Miskiat Badmus <badmusmiskiat@gmail.com>
2022-11-30 00:20:04 +0100

    returns button

--- src/components/Investment.tsx
+++ src/components/Investment.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Investments = () => {
   return (
-    <div>Investments</div>
-  )
-}
+    <div className="investment">
+      {/* <img
+        src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/invest_c3fcc60df0/invest_c3fcc60df0.png"
+        style="opacity: 1;"
+      /> */}
+      <button className="returns">Up To 25% Returns</button>
+      <h1>Access investment opportunities</h1>
+      <p>
+        Invest securely and confidently on the go. Grow your money confidently
+        by investing in pre-vetted investment opportunities.
+      </p>
+      <button>Learn more about Investments ></button>
+    </div>
+  );
+};
 
-export default Investments
+export default Investments;
