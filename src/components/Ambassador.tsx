47ee5962c83e17ac2c2fa5b8a680c29f4a7b242f
Miskiat Badmus <badmusmiskiat@gmail.com>
2022-12-04 11:11:08 +0100

    ambassador section

--- src/components/Ambassador.tsx
+++ src/components/Ambassador.tsx
@@ -3,13 +3,13 @@ import React from "react";
 const Ambassador = () => {
   return (
     <div className="ambassador">
-      <h1>Meet the saver of the month!</h1>
+      <p className="introduction">Meet the saver of the month!</p>
       <p>
         Every month, we shine a spotlight on one saver, asking them questions
         about their savings culture and how the product is specifically helping
         them shape how they spend and save for future responsibilities
       </p>
-      <button>Meet Tito</button>
+      <p className="button">Meet Tito</p>
     </div>
   );
 };
