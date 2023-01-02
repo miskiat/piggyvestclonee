217455b90b21f2d334d4ea252244117759840bb9
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-01-02 23:21:23 +0100

    fratures

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -15,14 +15,7 @@ const Features = () => {
         />
         <img
           className="newslink"
-          src="piggyvest.com/_next/image?url=https%3A%2F%2Fstorage.googleapis.com%2Fpiggyvestwebsite%2Fpiggywebsite2020%2Fbrand1_b2a9805fff%2Fbrand1_b2a9805fff.png&w=384&q=75"
-        />
-        <img
-          alt="brand"
-          src="/_next/image?url=https%3A%2F%2Fstorage.googleapis.com%2Fpiggyvestwebsite%2Fpiggywebsite2020%2Fbrand1_b2a9805fff%2Fbrand1_b2a9805fff.png&amp;w=384&amp;q=75"
-          decoding="async"
-          style="position:absolute;top:0;left:0;bottom:0;right:0;box-sizing:border-box;padding:0;border:none;margin:auto;display:block;width:0;height:0;min-width:100%;max-width:100%;min-height:100%;max-height:100%"
-          srcset="/_next/image?url=https%3A%2F%2Fstorage.googleapis.com%2Fpiggyvestwebsite%2Fpiggywebsite2020%2Fbrand1_b2a9805fff%2Fbrand1_b2a9805fff.png&amp;w=256&amp;q=75 1x, /_next/image?url=https%3A%2F%2Fstorage.googleapis.com%2Fpiggyvestwebsite%2Fpiggywebsite2020%2Fbrand1_b2a9805fff%2Fbrand1_b2a9805fff.png&amp;w=384&amp;q=75 2x"
+          src="https://www.piggyvest.com/_next/image?url=https%3A%2F%2Fstorage.googleapis.com%2Fpiggyvestwebsite%2Fpiggywebsite2020%2Fbrand1_b2a9805fff%2Fbrand1_b2a9805fff.png&w=384&q=75"
         />
       </div>
     </div>
