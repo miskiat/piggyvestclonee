670160b4d1e2e9160599b1572c1beda276a1e4c1
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-01-03 22:49:50 +0100

    footers

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,6 +6,41 @@ const Footer = () => {
       <div className="logo-links">
         <img src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/logo_08e6965627/logo_08e6965627.svg" />
       </div>
+      <div>
+        <div>Products</div>
+        <p>piggyvest</p>
+        <p>Invest</p>
+        <p>Safelock</p>
+        <p>Target Savings</p>
+        <p>Flex Naira</p>
+      </div>
+      <div>
+        <div>Company</div>
+        <p>About</p>
+        <p>FAQs</p>
+        <p>Blog</p>
+      </div>
+      <div>
+        <div>Legal</div>
+        <p>Terms</p>
+        <p>Privacy</p>
+        <p>Security</p>
+      </div>
+      <div>
+        <div>Products</div>
+        <p>
+          Tesmot house, Abdulrahman Okene close, Victoria Island,Lagos,Nigeria .
+        </p>
+        <p>contact@piggyvest.com</p>
+        <p>+234 700 933 933 933</p>
+      </div>
+      <div>
+        Piggyvest (formerly piggybank.ng) is the leading online savings &
+        investing platform in Nigeria. For over 6 years, our customers have
+        saved and invested billions of Naira that they would normally be tempted
+        to spend.
+      </div>
+      <div>2016 - 2022 PiggyTech Global Limited - RC 1405222</div>
     </div>
   );
 };
