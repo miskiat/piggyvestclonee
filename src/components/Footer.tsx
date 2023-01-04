11e442e3c9933f0899db4fca0c899b4f3cbb701b
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-01-04 22:00:05 +0100

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,34 +6,38 @@ const Footer = () => {
       <div className="logo-links">
         <img src="https://storage.googleapis.com/piggyvestwebsite/piggywebsite2020/logo_08e6965627/logo_08e6965627.svg" />
       </div>
-      <div>
-        <div>Products</div>
-        <p>piggyvest</p>
-        <p>Invest</p>
-        <p>Safelock</p>
-        <p>Target Savings</p>
-        <p>Flex Naira</p>
+      <div className="footer">
+        <div className="product-footer">
+          <div>Products</div>
+          <p>piggyvest</p>
+          <p>Invest</p>
+          <p>Safelock</p>
+          <p>Target Savings</p>
+          <p>Flex Naira</p>
+        </div>
+        <div>
+          <div>Company</div>
+          <p>About</p>
+          <p>FAQs</p>
+          <p>Blog</p>
+        </div>
+        <div>
+          <div>Legal</div>
+          <p>Terms</p>
+          <p>Privacy</p>
+          <p>Security</p>
+        </div>
+        <div>
+          <div>Products</div>
+          <p>
+            Tesmot house, Abdulrahman Okene close, Victoria Island,Lagos,Nigeria
+            .
+          </p>
+          <p>contact@piggyvest.com</p>
+          <p>+234 700 933 933 933</p>
+        </div>
       </div>
-      <div>
-        <div>Company</div>
-        <p>About</p>
-        <p>FAQs</p>
-        <p>Blog</p>
-      </div>
-      <div>
-        <div>Legal</div>
-        <p>Terms</p>
-        <p>Privacy</p>
-        <p>Security</p>
-      </div>
-      <div>
-        <div>Products</div>
-        <p>
-          Tesmot house, Abdulrahman Okene close, Victoria Island,Lagos,Nigeria .
-        </p>
-        <p>contact@piggyvest.com</p>
-        <p>+234 700 933 933 933</p>
-      </div>
+
       <div>
         Piggyvest (formerly piggybank.ng) is the leading online savings &
         investing platform in Nigeria. For over 6 years, our customers have
